8e35112279bc3be3672abccec84963639cb0f468
Mel DeJesus <meldejesus@mels-mbp.attlocal.net>
2018-05-29 11:54:30 -0400

    added / route

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const express = require('express')
 
 const app = express()
 
+const path = require("path");
+
 app.use(express.static('public'))
 
 // app.use('/static', express.static(path.join(__dirname, 'public')))
 // app.get('/', (req, res) => res.send('Hello World!'))
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
