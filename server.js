f60b6fa8abb7d89dc95102c9bd8051c523cc9dca
Mel DeJesus <meldejesus@mels-mbp.attlocal.net>
2018-05-29 12:23:54 -0400

    added / route

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express()
 
 const path = require("path");
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+
+app.use(express.static("./public"));
 
 // app.use('/static', express.static(path.join(__dirname, 'public')))
 // app.get('/', (req, res) => res.send('Hello World!'))
