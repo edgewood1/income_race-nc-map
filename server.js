c6887fde17ff8cb0cc28fc30d007d75b610dd9a6
Mel DeJesus <meldejesus@mels-mbp.attlocal.net>
2018-05-29 12:30:41 -0400

    added / route

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const app = express()
 
 const path = require("path");
 
+var bodyParser = require("body-parser");
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
 // app.use(express.static('public'))
 
 app.use(express.static("./public"));
 
-// app.use('/static', express.static(path.join(__dirname, 'public')))
 // app.get('/', (req, res) => res.send('Hello World!'))
 
   // index route loads view.html
   app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/index", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
