661335783093de848477ef7d3254f2e418d4ad0a
Mel DeJesus <meldejesus@mels-mbp.attlocal.net>
2018-05-29 11:48:32 -0400

    restructuring files

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
